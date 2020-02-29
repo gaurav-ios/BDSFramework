b2f2b8f01e533ee4bd9d3e1616805dac260f591b
Gaurav Kumawat <gauravsk01@gmail.com>
2020-02-29 12:43:26 +0530

    ad
    added all new files

---
+++ BDSFramework/BDSFramework.h
@@ -0,0 +1,19 @@
+//
+//  BDSFramework.h
+//  BDSFramework
+//
+//  Created by Raksha Saini on 28/02/20.
+//  Copyright © 2020 Ramss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BDSFramework.
+FOUNDATION_EXPORT double BDSFrameworkVersionNumber;
+
+//! Project version string for BDSFramework.
+FOUNDATION_EXPORT const unsigned char BDSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BDSFramework/PublicHeader.h>
+
+
